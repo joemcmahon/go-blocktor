94aef80c7dd7668f1b4796ef597f6cade1074983
Paulo Roberto <paulomaxlider@gmail.com>
2017-08-29 10:13:31 -0300

    fix

---
+++ blocktor.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"strings"
+)
+
+// UpdateAddresses get all addresses from the past 16 hours.
+func UpdateAddresses() []string {
+	response, _ := http.Get("https://check.torproject.org/cgi-bin/TorBulkExitList.py?ip=1.1.1.1")
+	defer response.Body.Close()
+
+	body, _ := ioutil.ReadAll(response.Body)
+	result := strings.Split(string(body), "\n")
+
+	return result
+}
+
+func main() {
+	fmt.Println("Updating TOR list addresses...")
+
+	allAddresses := UpdateAddresses()
+
+	for _, addr := range allAddresses {
+		fmt.Println(addr)
+	}
+}
