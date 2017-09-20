adafb12b155c7dd8134d0edad1959df939ef3a0e
Paulo Roberto <paulomaxlider@gmail.com>
2017-09-20 10:43:46 -0300

    fix

--- blocktor.go
+++ blocktor.go
@@ -1,39 +1,74 @@
 package main
 
 import (
-	"flag"
-	"fmt"
-	"runtime"
+  "io/ioutil"
+  "net/http"
+  "strings"
+  "fmt"
+  "flag"
+  "runtime"
+  "os/exec"
 )
 
-func main() {
-	if runtime.GOOS != "linux" {
-		fmt.Println("Operating system not supported.")
-		return
+// Exec execute the commands.
+func Exec(cmd string, args ...string) {
+	c := exec.Command(cmd, args...)
+	err := c.Run()
+
+	if err != nil {
+		panic(err)
+	}
+}
+
+// UpdateFromTOR get all addresses from the past 16 hours.
+func UpdateFromTOR(address string) []string {
+	url := "https://check.torproject.org/cgi-bin/TorBulkExitList.py?ip="
+	res, errGet := http.Get(url + address)
+
+	if errGet != nil {
+		panic(errGet)
 	}
 
-	flagA := flag.String("address", "1.1.1.1", "IP Address")
-	flagR := flag.String("rules", "./start.sh", "Script initial rules")
-	flagC := flag.String("clear", "false", "Clear rules")
+	defer res.Body.Close()
+	body, errBody := ioutil.ReadAll(res.Body)
+
+	if errBody != nil {
+		panic(errBody)
+	}
+
+	result := strings.Split(string(body), "\n")
+	return result
+}
+
+func main(){
+  if runtime.GOOS != "linux" {
+    fmt.Println("Operating system not supported.")
+    return
+  }
+
+  flagAddress := flag.String("address", "1.1.1.1", "IP Address to block TOR network")
+	flagRules := flag.String("rules", "", "Custom script with initial rules")
 
 	flag.Parse()
 
-	fmt.Println("Updating...")
-	TORaddresses := UpdateFromTOR(*flagA)
+  fmt.Println("Updating...")
+  TORaddresses := UpdateFromTOR(*flagAddress)
 
-	if *flagC == "true" {
-		fmt.Println("Cleaning...")
-		ClearBlockTOR()
-	} else {
-		ClearAll()
-		InitialRules(*flagR)
+  fmt.Println("Cleaning...")
 
-		fmt.Println("Blocking...")
+  Exec("iptables", "-F")
+  Exec("iptables", "-t", "nat", "-F")
 
-		for _, addr := range TORaddresses {
-			if len(addr) < 16 && len(addr) > 7 {
-				BlockRequestFrom(addr)
-			}
-		}
-	}
+  fmt.Println("Initial rules...")
+  if *flagRules != ""{
+    Exec(*flagRules)
+  }
+
+  fmt.Println("Blocking...")
+
+  for _, address := range TORaddresses {
+    if len(address) < 16 && len(address) > 7 {
+      Exec("iptables", "-A", "INPUT", "-s", address, "-j", "DROP")
+    }
+  }
 }
